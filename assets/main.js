5f65eaa31a0a19f1ad0a2d78611535f1a836258f
Aleksey Vaskin <alzor85@gmail.com>
2023-10-01 17:54:04 +0300

    feat: finish accordion pet project

--- assets/main.js
+++ assets/main.js
@@ -7,9 +7,23 @@ function accordion() {
     const content = document.querySelectorAll('.accordion__content');
 
     triggers.forEach((trigger, idx) => {
+        if(trigger.parentNode.classList.contains('active')) slideDown(content[idx]);
+        
         trigger.addEventListener('click',() => {
             const parent = trigger.parentNode;
             parent.classList.toggle('active');
+
+            if(parent.classList.contains('active')) {
+                slideDown(content[idx]);
+                items.forEach((item,i) => {
+                    if (i !== idx) {
+                        slideUp(content[i]);
+                        item.classList.remove('active');
+                    }
+                });
+            } else {
+                slideUp(content[idx]);
+            }
         });
     });
 }
